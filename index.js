1fe408790c8900a018b040e9ceee8dafface41bc
kastrowalker <victorcsa2002@gmail.com>
2021-03-24 22:21:35 -0300

    Versao 1.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const tmi = require("tmi.js");
 const dotenv = require("dotenv").config();
+const axios = require("axios");
+const fs = require("fs");
 
 const BotName = process.env.NOME_BOT;
 const ChannelName = process.env.NOME_CANAL;
@@ -11,19 +13,78 @@ const opts = {
   channels: [ChannelName],
 };
 const client = new tmi.client(opts);
+const streamers = JSON.parse(
+  fs.readFileSync("./data/streamers.json", { encoding: "utf8", flag: "r" })
+);
+const streamersDaCaverna = JSON.parse(
+  fs.readFileSync("./data/streamersDaCaverna.json", {
+    encoding: "utf8",
+    flag: "r",
+  })
+);
+const streamersOn = [];
+const tempoDivulgacao = 600000;
+const tempoAtualizacao = 500000;
 
 function recivedMessage(target, context, msg, bot) {
   if (bot) {
     return;
   }
-
-  if (msg == "!caverna") {
-    client.say(
-      target,
-      "/me Uma comunidade voltada para programação em geral com o objetivo de ajudar uns aos outros, estudar coletivamente, e outros. https://discord.io/caverna ⭐Por favor, não se esqueça de passar no canal #regras para liberar o acesso á todas as salas do nosso servidor⭐"
-    );
-  }
 }
+
+function atualizaStreamers() {
+  streamers.forEach((streamer) => {
+    axios
+      .get("https://api.twitch.tv/kraken/streams/" + streamer.id, {
+        headers: {
+          Accept: "application/vnd.twitchtv.v5+json",
+          "Client-ID": process.env.CLIENT_ID,
+        },
+      })
+      .then(function (response) {
+        const nomeStreamer = streamer.name;
+
+        if (response.data.stream) {
+          if (!streamersOn.includes(nomeStreamer)) {
+            streamersOn.push(nomeStreamer);
+          }
+        } else {
+          if (streamersOn.includes(nomeStreamer)) {
+            streamersOn.splice(streamersOn.indexOf(nomeStreamer), 1);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+}
+
+function divulgaStreamer() {
+  let streamerSorteado =
+    streamersDaCaverna[Math.floor(Math.random() * streamersDaCaverna.length)];
+
+  streamersOn.forEach((streamer) => {
+    if (streamer !== streamerSorteado.name) {
+      client.say(
+        streamer,
+        `/me ${streamerSorteado.description}. Conheça o canal https://twitch.tv/${streamerSorteado.name}`
+      );
+      client.say(streamer, `!sh-so ${streamerSorteado.name}`);
+    }
+  });
+}
+
+setInterval(() => {
+  atualizaStreamers();
+}, tempoAtualizacao);
+
+setInterval(() => {
+  divulgaStreamer();
+}, tempoDivulgacao);
+
+clearInterval(atualizaStreamers);
+clearInterval(divulgaStreamer);
 
 client.on("message", recivedMessage);
 client.on("connected", () => {
